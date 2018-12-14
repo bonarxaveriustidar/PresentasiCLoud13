e29589190f314a5ff5e88a573c781cf6fc516b09
osboxes.org <bonarxaveriustidar@gmail.com>
2018-12-14 05:55:01 -0500

    Commit Pertama

---
+++ NegoOnline/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Nego Online, Jual Beli Barang Nego Terus Sampai Jadi</title>
+<style type="text/css">
+<!--
+body {
+	background-image: url(image/background.jpg);
+	background-size: 100% 100%;
+	min-height: 700px;
+}
+-->
+</style></head>
+
+<body>
+<table width="793" border="2">
+  <tr>
+    <td height="83" colspan="5"><a href="index.php"><img src="image/title1.jpg" width="800" height="117" border="0" /></a></td>
+  </tr>
+  <tr>
+    <td width="90"><div align="center">Pencarian</div></td>
+    <td width="185"><form id="form1" name="form1" method="post" action="">
+      <div align="center">Kategori</div>
+    </form>
+    </td>
+    <td width="156"><form id="form2" name="form2" method="post" action="">
+      <div align="center">Katalog Terbaru 
+      </div>
+    </form>
+    
+    </td>
+    <td width="183"><form id="form3" name="form3" method="post" action="">
+      <div align="center">Login Penjual 
+      </div>
+    </form>
+    </td>
+    <td width="162"><form id="form4" name="form4" method="post" action="">
+      <div align="center">Register Penjual 
+      </div>
+    </form>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
